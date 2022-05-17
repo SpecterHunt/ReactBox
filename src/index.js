1a3439c216ad647989f70b0391fb8762306c3514
Ayush Tripathi <66113530+SpecterHunt@users.noreply.github.com>
2022-05-17 15:31:15 +0530

    New changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+const name = "Ayush";
+const year = new Date().getFullYear();
+ReactDom.render(
+  <div>
+    <p>Created by {name}</p>
+    <p>Copyright {year}</p>
+  </div>,
+  document.getElementById("root")
+);
 //Create a react app from scratch.
 //It should display 2 paragraph HTML elements.
 //The paragraphs should say:
